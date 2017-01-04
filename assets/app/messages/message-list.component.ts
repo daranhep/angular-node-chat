d16da6e2604f788d4001aa92460e54b2195c1edc
Dara Nhep <dara.h.nhep@gmail.com>
2017-01-04 13:42:55 -0600

    updated getMessages function to connect to backend

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -4,8 +4,8 @@ import { Message } from "./message.model";
 import { MessageService } from "./message.service";
 
 @Component({
-  selector: 'app-message-list',
-  template: `
+    selector: 'app-message-list',
+    template: `
     <div class="col-md-8 col-md-offset-2">
       <app-message
         [message]="message"
@@ -15,12 +15,17 @@ import { MessageService } from "./message.service";
   `
 })
 export class MessageListComponent implements OnInit {
-  messages: Message[];
+    messages: Message[];
 
-  constructor(private messageService: MessageService) {}
+    constructor(private messageService: MessageService) { }
 
-  ngOnInit() {
-    this.messages = this.messageService.getMessages();
-  }
+    ngOnInit() {
+        this.messageService.getMessages()
+            .subscribe(
+            (messages: Message[]) => {
+                this.messages = messages;
+            }
+            );
+    }
 
 }
